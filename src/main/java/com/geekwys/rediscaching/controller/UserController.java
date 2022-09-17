acb047f4dad9ae391ee7f99fb45c3cf464f26e7d
sbmwangi <shem.mwangi@cellulant.io>
2022-09-17 15:01:35 +0300

    register new users to local database

--- src/main/java/com/geekwys/rediscaching/controller/UserController.java
+++ src/main/java/com/geekwys/rediscaching/controller/UserController.java
@@ -2,11 +2,17 @@ package com.geekwys.rediscaching.controller;
 
 import com.geekwys.rediscaching.model.User;
 import com.geekwys.rediscaching.repository.UserRepository;
+import com.geekwys.rediscaching.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author shem.mwangi
@@ -15,17 +21,22 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    final UserRepository userRepository;
+    final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
 
-    @GetMapping("/users")
+    @GetMapping("/get/users")
     public List<User> fetchAllUsers() {
-        log.info("Fetch all users in org: {}", userRepository.findAll());
-        return userRepository.findAll();
+        log.info("Fetch all users in org: {}", userService.fetchAllUserRegisteredInOrganization());
+        return userService.fetchAllUserRegisteredInOrganization();
+    }
+
+    @PostMapping("/add/users")
+    public void addNewUsers(@RequestBody User user) {
+        userService.registerNewUserToOrganization(user);
     }
 
 }
--- src/main/java/com/geekwys/rediscaching/repository/UserRepository.java
+++ src/main/java/com/geekwys/rediscaching/repository/UserRepository.java
@@ -4,15 +4,10 @@ import com.geekwys.rediscaching.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 /**
  * @author shem.mwangi
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    //custom query to search for user by first name or second name
-    List<User> findUserByFirstNameAndLastName(String firstName, String lastName);
 
 }
