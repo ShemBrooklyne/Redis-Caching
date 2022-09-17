590332ea0037d48081dd5b5ba4a29dfa9148b40f
sbmwangi <shem.mwangi@cellulant.io>
2022-09-17 15:14:17 +0300

    Initiate redis connection

--- src/main/java/com/geekwys/rediscaching/RedisCachingApplication.java
+++ src/main/java/com/geekwys/rediscaching/RedisCachingApplication.java
@@ -1,7 +1,12 @@
 package com.geekwys.rediscaching;
 
+import com.geekwys.rediscaching.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * @author shem.mwangi
@@ -11,6 +16,20 @@ public class RedisCachingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisCachingApplication.class, args);
+    }
+
+    //initiate new redis connection
+    @Bean
+    RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    //initiating RedisTemplate for entity users
+    @Bean
+    RedisTemplate<String, User> redisTemplate() {
+        RedisTemplate<String, User> userRedisTemplate = new RedisTemplate<>();
+        userRedisTemplate.setConnectionFactory(redisConnectionFactory());
+        return userRedisTemplate;
     }
 
 }
