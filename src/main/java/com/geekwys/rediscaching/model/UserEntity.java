c816207869ab96b891316fdf9f8771b8f2a4dc19
sbmwangi <shem.mwangi@cellulant.io>
2022-09-17 13:29:18 +0300

    replaced lombok annotation with hashcode to improve on performance

--- src/main/java/com/geekwys/rediscaching/model/UserEntity.java
+++ src/main/java/com/geekwys/rediscaching/model/UserEntity.java
@@ -1,18 +1,21 @@
 package com.geekwys.rediscaching.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 /**
  * @author shem.mwangi
  */
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -23,4 +26,17 @@ public class UserEntity {
 
     private String firstName;
     private String lastName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        UserEntity that = (UserEntity) o;
+        return userID != null && Objects.equals(userID, that.userID);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
