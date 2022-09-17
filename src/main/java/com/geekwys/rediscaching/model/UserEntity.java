6740b5a30760f58311cd3c30ed27c0392f24ef52
sbmwangi <shem.mwangi@cellulant.io>
2022-09-17 13:00:40 +0300

    added spring ID attribute to entity class

--- src/main/java/com/geekwys/rediscaching/model/UserEntity.java
+++ src/main/java/com/geekwys/rediscaching/model/UserEntity.java
@@ -4,14 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author shem.mwangi
  */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long userID;
+
     private String firstName;
     private String lastName;
 }
