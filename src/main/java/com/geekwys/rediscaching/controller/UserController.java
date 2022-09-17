38a892241ba814ae21acd2ed2f26f4ab56d6d8ec
sbmwangi <shem.mwangi@cellulant.io>
2022-09-17 13:06:16 +0300

    implemented controller for repository and added high-level logging

---
+++ src/main/java/com/geekwys/rediscaching/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.geekwys.rediscaching.controller;
+
+import com.geekwys.rediscaching.model.UserEntity;
+import com.geekwys.rediscaching.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author shem.mwangi
+ */
+@Slf4j
+@RestController
+public class UserController {
+
+    final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public List<UserEntity> fetchAllUsers() {
+        log.info("Fetch all users in org: {}", userRepository.findAll());
+        return userRepository.findAll();
+    }
+
+}
