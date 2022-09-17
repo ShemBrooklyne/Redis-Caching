6e56bd63dda92221c71b03a16e8555e44c6501dd
sbmwangi <shem.mwangi@cellulant.io>
2022-09-17 12:52:43 +0300

    configured mysql connection

--- src/main/java/com/geekwys/rediscaching/RedisCachingApplication.java
+++ src/main/java/com/geekwys/rediscaching/RedisCachingApplication.java
@@ -3,6 +3,9 @@ package com.geekwys.rediscaching;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author shem.mwangi
+ */
 @SpringBootApplication
 public class RedisCachingApplication {
 
