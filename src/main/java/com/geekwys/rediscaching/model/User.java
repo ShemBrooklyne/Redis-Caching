a8fcc91e1c4f1fb542517bf77940a5ac9a64cc66
sbmwangi <shem.mwangi@cellulant.io>
2022-09-19 11:28:44 +0300

    defined redis hash annotation and cache access key in dto

--- src/main/java/com/geekwys/rediscaching/model/User.java
+++ src/main/java/com/geekwys/rediscaching/model/User.java
@@ -1,9 +1,9 @@
 package com.geekwys.rediscaching.model;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.redis.core.RedisHash;
 
 import javax.persistence.*;
 
@@ -14,11 +14,12 @@ import javax.persistence.*;
 @Getter
 @RequiredArgsConstructor
 @Entity(name = "users")
+@RedisHash("organization.users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Long userID;
+    @org.springframework.data.annotation.Id private Long userID;
 
     @Column(name = "first_name")
     private String firstName;
